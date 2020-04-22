7fec8acd67dde95df7a6f4e84c7caa051cd1f385
Yifan Wu <tyivanwu@gmail.com>
2020-04-21 23:08:45 -0400

    refactor: abstract search-bar from nav

--- src/components/nav.ts
+++ src/components/nav.ts
@@ -1,12 +1,12 @@
 import { AppContext, StatusType } from "../api";
 import { ABOUT, CONTACT } from "../routes";
-import { TOGGLE_NAV, SET_INPUT, GET_ENTRY, SET_STATUS } from "../events";
+import { TOGGLE_NAV } from "../events";
 
 import { logo } from "./logo";
 import { routeLink } from "./route-link";
 import { eventBtn } from "./event-btn";
+import { searchBar } from "./search-bar";
 import {
-    SEARCH,
     HEADER_HAMBURGER,
     HEADER_CLOSE,
     INFO_OUTLINE,
@@ -19,9 +19,7 @@ import {
  */
 export function nav(ctx: AppContext) {
     const isNavOpen = ctx.views.isNavOpen.deref()!;
-    const input = ctx.views.input.deref()!;
     const ui = ctx.ui.nav;
-    const bus = ctx.bus;
     return [
         "nav",
         {
@@ -42,60 +40,7 @@ export function nav(ctx: AppContext) {
                 ],
             ],
         ],
-        [
-            "div",
-            {
-                class: "w-full xl:px-24 px-4 pt-2 pb-6",
-            },
-            [
-                "div",
-                { class: "relative" },
-                [
-                    "input",
-                    {
-                        id: "entry_search",
-                        class:
-                            "transition bg-white shadow-md focus:outline-0 border border-transparent placeholder-gray-700 rounded-lg py-2 pr-4 pl-10 block w-full appearance-none leading-normal",
-                        placeholder: "搜寻条目 / Search Entries",
-                        type: "text",
-                        oninput: (e) => {
-                            bus.dispatch([SET_INPUT, e.target.value]);
-                        },
-                        onkeyup: (e) => {
-                            if (e.key === "Enter") {
-                                ctx.bus.dispatch(
-                                    ctx.views.entries.deref()![input]
-                                        ? [
-                                              SET_STATUS,
-                                              [
-                                                  StatusType.SUCCESS,
-                                                  "loaded from cache",
-                                                  true,
-                                              ],
-                                          ]
-                                        : [GET_ENTRY, input]
-                                );
-                            }
-                        },
-                    },
-                ],
-                [
-                    "div",
-                    {
-                        class:
-                            "pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center",
-                    },
-                    [
-                        "div",
-                        {
-                            class:
-                                "fill-current pointer-events-none text-gray-600 w-4 h-4",
-                        },
-                        SEARCH,
-                    ],
-                ],
-            ],
-        ],
+        searchBar,
         [
             "div",
             isNavOpen ? ui.inner.open : ui.inner.close,
---
+++ src/components/search-bar.ts
@@ -0,0 +1,64 @@
+import { AppContext, StatusType } from "../api";
+import { SET_INPUT, GET_ENTRY, SET_STATUS } from "../events";
+
+import { SEARCH } from "@thi.ng/hiccup-carbon-icons";
+
+export function searchBar(ctx: AppContext) {
+    const bus = ctx.bus;
+    const input = ctx.views.input.deref()!;
+    const entries = ctx.views.entries.deref()!;
+    return [
+        "div",
+        {
+            class: "w-full xl:px-24 px-4 pt-2 pb-6",
+        },
+        [
+            "div",
+            { class: "relative" },
+            [
+                "input",
+                {
+                    id: "entry_search",
+                    class:
+                        "transition bg-white shadow-md focus:outline-0 border border-transparent placeholder-gray-700 rounded-lg py-2 pr-4 pl-10 block w-full appearance-none leading-normal",
+                    placeholder: "搜寻条目 / Search Entries",
+                    type: "text",
+                    oninput: (e) => {
+                        bus.dispatch([SET_INPUT, e.target.value]);
+                    },
+                    onkeyup: (e) => {
+                        if (e.key === "Enter") {
+                            bus.dispatch(
+                                entries[input]
+                                    ? [
+                                          SET_STATUS,
+                                          [
+                                              StatusType.SUCCESS,
+                                              "loaded from cache",
+                                              true,
+                                          ],
+                                      ]
+                                    : [GET_ENTRY, input]
+                            );
+                        }
+                    },
+                },
+            ],
+            [
+                "div",
+                {
+                    class:
+                        "pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center",
+                },
+                [
+                    "div",
+                    {
+                        class:
+                            "fill-current pointer-events-none text-gray-600 w-4 h-4",
+                    },
+                    SEARCH,
+                ],
+            ],
+        ],
+    ];
+}
