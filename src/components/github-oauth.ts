ee083b411a44953b6776ab325602ce7bed69e1d1
Yifan Wu <tyivanwu@gmail.com>
2020-05-04 22:41:55 -0400

    minor: clean up a bit

--- src/components/github-oauth.ts
+++ src/components/github-oauth.ts
@@ -2,8 +2,7 @@ import type { AppContext } from "../api";
 import { GET_TOKEN } from "../events";
 
 /**
- * Github OAuth callback page. Triggers JSON I/O request on init if entry
- * data has not been loaded yet.
+ * Github OAuth callback page.
  *
  * @param ctx injected context object
  */
@@ -11,11 +10,9 @@ export function githubOauth(ctx: AppContext) {
     // TODO: make the extracting more robust
     const code = window.location.href.split("?code=")[1].split("#/")[0];
     ctx.bus.dispatch([GET_TOKEN, code]);
-    return () => {
-        return [
-            "div",
-            { class: "text-4xl m-auto p-6" },
-            "Signing-in with Github...",
-        ];
-    };
+    return [
+        "div",
+        { class: "text-4xl m-auto p-6" },
+        "Signing-in with Github...",
+    ];
 }
