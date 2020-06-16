5949b0a9567273378d22f56ecfc8602692c98fc2
Yifan Wu <tyivanwu@gmail.com>
2020-06-16 00:08:11 -0400

    feat: add zh faq

--- src/components/faq.ts
+++ src/components/faq.ts
@@ -1,22 +1,37 @@
 import { AppContext } from "../api";
+import type { IObjectOf } from "@thi.ng/api";
 
 interface aq {
     question: string;
     answer: string;
 }
 
-const aqs: aq[] = [
-    {
-        question: "Why I cannot login?",
-        answer:
-            "We are using cookies to store your JSON Web Token (JWT). If you block third-party cookies, the communications between your browser and our servers will not work properly.",
-    },
-    {
-        question: "Why I cannot search abbreviations?",
-        answer:
-            "Due to the complexity of searching entries, short keywords and abbreviations, especially with . (dot), sometimes can not be searchable, but you can try the full name or related keywords, such as Dr. Dre for N.W.A, to find the entry.",
-    },
-];
+const aqs: IObjectOf<aq[]> = {
+    en: [
+        {
+            question: "Why I cannot login?",
+            answer:
+                "We are using third-party cookies to store your login state, which helps us to verity the communications between your browser and our servers.",
+        },
+        {
+            question: "Why I cannot search abbreviations?",
+            answer:
+                "Due to the complexity of searching entries, short keywords and abbreviations, especially with . (dot), sometimes can not be searchable, but you can try the full name or related keywords, such as Dr. Dre for N.W.A, to find the entry.",
+        },
+    ],
+    zh: [
+        {
+            question: "为何无法登陆？",
+            answer:
+                "Mencius 使用第三方 cookie 来保存并验证用户登陆状态，请在当前页面允许使用第三方 cookie。",
+        },
+        {
+            question: "为何无法检索缩略语？",
+            answer:
+                "由于检索词条的复杂性，有些时候带有 .（点）的缩略语无法被搜索，但是可以通过搜索全名或者相关信息来找到目标词条：比如用 Dr. Dre 找到 N.W.A。",
+        },
+    ],
+};
 
 /**
  * FAQ page component.
@@ -55,8 +70,8 @@ export function faq(ctx: AppContext) {
                 "section",
                 [
                     "div",
-                    { class: "flex flex-col items-center px-12" },
-                    aqs.map((x) => [
+                    { class: "flex flex-col items-start px-12" },
+                    aqs.zh.map((x) => [
                         "article",
                         [
                             "h2",
