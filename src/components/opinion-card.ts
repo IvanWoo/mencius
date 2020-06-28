4ec1a2eb6c3e92d0f57d9fa87c155a139a6974f9
Yifan Wu <tyivanwu@gmail.com>
2020-06-27 23:39:27 -0400

    refactor: abstract modal into helper function

--- src/components/opinion-card.ts
+++ src/components/opinion-card.ts
@@ -15,11 +15,7 @@ import { voteZone } from "./vote-zone";
 import { withSize, EDIT, DELETE, WARNING } from "@thi.ng/hiccup-carbon-icons";
 import { dropdown, DropDownOption } from "@thi.ng/hdom-components";
 
-function model() {
-    // [frame, [content, footer], cancel_btn]
-}
-
-function modalFrame(content: any) {
+function modalFrame(ctx: AppContext, content: any, cancelBtn: any) {
     return [
         "div",
         { class: "fixed inset-0 flex items-center justify-center z-30" },
@@ -27,9 +23,150 @@ function modalFrame(content: any) {
             "div",
             {
                 class:
-                    "fixed flex flex-col items-center justify-between mt-2 py-2 md:w-1/3 w-2/4 bg-white rounded-lg shadow-xl z-40",
+                    "fixed flex flex-col items-center justify-between mt-2 pt-2 lg:w-1/3 md:w-1/2 w-3/4 bg-white rounded-lg shadow-xl z-40",
             },
             content,
+        ],
+        cancelBtn,
+    ];
+}
+
+function modal(ctx: AppContext, content: any, cancelBtn: any) {
+    return [modalFrame, content, cancelBtn];
+}
+
+function deleteModal(ctx: AppContext, opinion: Opinion) {
+    const views = ctx.views;
+    const id = decodeURI(ctx.views.route.deref()!.params.id);
+    const user = views.user.deref()!;
+    return [
+        modal,
+        [
+            [
+                "div",
+                {
+                    class:
+                        "block font-medium text-gray-800 text-lg p-4 border-b border-b-2 w-full",
+                },
+                ["div", { class: "mb-2" }, "Delete opinion"],
+                [
+                    "div",
+                    {
+                        class: "text-sm text-gray-500",
+                    },
+                    "Are you sure you want to delete your opinion? This action cannot be undone.",
+                ],
+            ],
+            [
+                "div",
+                {
+                    class: "flex flex-row self-end m-2",
+                },
+                [
+                    eventBtn,
+                    [CLOSE_DELETE_OPINION],
+                    {
+                        class:
+                            "block px-4 py-2 text-black text-left font-medium",
+                    },
+                    "CANCEL",
+                ],
+                [
+                    eventBtn,
+                    [
+                        DELETE_OPINION,
+                        <OpinionMessenger>{
+                            id,
+                            data: opinion,
+                            userName: user.login,
+                        },
+                    ],
+                    {
+                        class:
+                            "block px-4 py-2 text-white text-left bg-red-600 rounded-lg font-medium hover:bg-red-500 transition ease-in-out duration-150",
+                    },
+                    "DELETE",
+                ],
+            ],
+        ],
+        [
+            eventBtn,
+            [CLOSE_DELETE_OPINION],
+            {
+                tabindex: "-1",
+                class:
+                    "fixed inset-0 h-full w-full cursor-default bg-black bg-opacity-50 z-20",
+            },
+        ],
+    ];
+}
+
+function reportModal(ctx: AppContext) {
+    const bus = ctx.bus;
+    const report = ctx.views.report.deref()!;
+    return [
+        modal,
+        [
+            [
+                "div",
+                {
+                    class:
+                        "block font-medium text-gray-800 text-lg p-4 border-b border-b-2 w-full",
+                },
+                ["div", { class: "mb-2" }, "Report opinion"],
+                [
+                    dropdown,
+                    {
+                        class:
+                            "shadow-inner appearance-none border rounded w-full py-2 px-3 bg-gray-200 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:bg-white",
+                        onchange: (e: InputEvent) =>
+                            bus.dispatch([
+                                SET_REPORT,
+                                {
+                                    key: "reason",
+                                    value: (<HTMLTextAreaElement>e.target)
+                                        .value,
+                                },
+                            ]),
+                    },
+                    LANG_OPTS,
+                    report.reason,
+                ],
+            ],
+            [
+                "div",
+                {
+                    class: "flex flex-row self-end m-2",
+                },
+                [
+                    eventBtn,
+                    [CLOSE_REPORT],
+                    {
+                        class:
+                            "block px-4 py-2 text-black text-left font-medium",
+                    },
+                    "CANCEL",
+                ],
+                [
+                    eventBtn,
+                    [CREATE_REPORT, { data: report }],
+                    {
+                        class:
+                            "block px-4 py-2 text-black text-left font-medium disabled:opacity-50 transition ease-in-out duration-300",
+                        disabled: report.reason ? false : true,
+                    },
+                    "REPORT",
+                ],
+            ],
+        ],
+        [
+            eventBtn,
+            [CLOSE_REPORT],
+            {
+                tabindex: "-1",
+                class:
+                    "fixed inset-0 h-full w-full cursor-default bg-black bg-opacity-50",
+            },
         ],
     ];
 }
@@ -51,12 +188,10 @@ const LANG_OPTS: DropDownOption[] = [
 
 export function opinionCard(ctx: AppContext, opinion: Opinion) {
     const views = ctx.views;
-    const bus = ctx.bus;
     const id = decodeURI(ctx.views.route.deref()!.params.id);
     const user = views.user.deref()!;
     const deleteOpinionOpen = ctx.views.deleteOpinionOpen.deref()!;
     const reportOpen = ctx.views.reportOpen.deref()!;
-    const report = ctx.views.report.deref()!;
     return [
         "div",
         {
@@ -103,7 +238,7 @@ export function opinionCard(ctx: AppContext, opinion: Opinion) {
                         // ],
                     ],
                 ],
-                // TODO: refactor modal into separate function
+                // interactive with other opinion
                 user.login == opinion.github_handle
                     ? [
                           "div",
@@ -160,83 +295,7 @@ export function opinionCard(ctx: AppContext, opinion: Opinion) {
                                   ["div", "delete"],
                               ],
                           ],
-                          deleteOpinionOpen
-                              ? [
-                                    "div",
-                                    {
-                                        class:
-                                            "fixed inset-0 flex items-center justify-center z-30",
-                                    },
-                                    [
-                                        "div",
-                                        {
-                                            class:
-                                                "fixed flex flex-col items-center justify-between mt-2 pt-2 lg:w-1/3 md:w-1/2 w-3/4 bg-white rounded-lg shadow-xl z-40",
-                                        },
-                                        [
-                                            "div",
-                                            {
-                                                class:
-                                                    "block font-medium text-gray-800 text-lg p-4 border-b border-b-2 w-full",
-                                            },
-                                            [
-                                                "div",
-                                                { class: "mb-2" },
-                                                "Delete opinion",
-                                            ],
-                                            [
-                                                "div",
-                                                {
-                                                    class:
-                                                        "text-sm text-gray-500",
-                                                },
-                                                "Are you sure you want to delete your opinion? This action cannot be undone.",
-                                            ],
-                                        ],
-                                        [
-                                            "div",
-                                            {
-                                                class:
-                                                    "flex flex-row self-end m-2",
-                                            },
-                                            [
-                                                eventBtn,
-                                                [CLOSE_DELETE_OPINION],
-                                                {
-                                                    class:
-                                                        "block px-4 py-2 text-black text-left font-medium",
-                                                },
-                                                "CANCEL",
-                                            ],
-                                            [
-                                                eventBtn,
-                                                [
-                                                    DELETE_OPINION,
-                                                    <OpinionMessenger>{
-                                                        id,
-                                                        data: opinion,
-                                                        userName: user.login,
-                                                    },
-                                                ],
-                                                {
-                                                    class:
-                                                        "block px-4 py-2 text-white text-left bg-red-600 rounded-lg font-medium hover:bg-red-500 transition ease-in-out duration-150",
-                                                },
-                                                "DELETE",
-                                            ],
-                                        ],
-                                    ],
-                                    [
-                                        eventBtn,
-                                        [CLOSE_DELETE_OPINION],
-                                        {
-                                            tabindex: "-1",
-                                            class:
-                                                "fixed inset-0 h-full w-full cursor-default bg-black bg-opacity-50 z-20",
-                                        },
-                                    ],
-                                ]
-                              : [],
+                          deleteOpinionOpen ? [deleteModal, opinion] : [],
                       ]
                     : [
                           "div",
@@ -270,94 +329,7 @@ export function opinionCard(ctx: AppContext, opinion: Opinion) {
                                   ["div", "report"],
                               ],
                           ],
-                          reportOpen
-                              ? [
-                                    "div",
-                                    {
-                                        class:
-                                            "fixed inset-0 flex items-center justify-center z-30",
-                                    },
-                                    [
-                                        "div",
-                                        {
-                                            class:
-                                                "fixed flex flex-col items-center justify-between mt-2 pt-2 md:w-1/3 w-2/4 bg-white rounded-lg shadow-xl z-40",
-                                        },
-                                        [
-                                            "div",
-                                            {
-                                                class:
-                                                    "block font-medium text-gray-800 text-lg p-4 border-b border-b-2 w-full",
-                                            },
-                                            [
-                                                "div",
-                                                { class: "mb-2" },
-                                                "Report opinion",
-                                            ],
-                                            [
-                                                dropdown,
-                                                {
-                                                    class:
-                                                        "shadow-inner appearance-none border rounded w-full py-2 px-3 bg-gray-200 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:bg-white",
-                                                    onchange: (e: InputEvent) =>
-                                                        bus.dispatch([
-                                                            SET_REPORT,
-                                                            {
-                                                                key: "reason",
-                                                                value: (<
-                                                                    HTMLTextAreaElement
-                                                                >e.target)
-                                                                    .value,
-                                                            },
-                                                        ]),
-                                                },
-                                                LANG_OPTS,
-                                                report.reason,
-                                            ],
-                                        ],
-                                        [
-                                            "div",
-                                            {
-                                                class:
-                                                    "flex flex-row self-end m-2",
-                                            },
-                                            [
-                                                eventBtn,
-                                                [CLOSE_REPORT],
-                                                {
-                                                    class:
-                                                        "block px-4 py-2 text-black text-left font-medium",
-                                                },
-                                                "CANCEL",
-                                            ],
-                                            [
-                                                eventBtn,
-                                                [
-                                                    CREATE_REPORT,
-                                                    { data: report },
-                                                ],
-                                                {
-                                                    class:
-                                                        "block px-4 py-2 text-black text-left font-medium disabled:opacity-50 transition ease-in-out duration-300",
-                                                    disabled: report.reason
-                                                        ? false
-                                                        : true,
-                                                },
-                                                "REPORT",
-                                            ],
-                                        ],
-                                    ],
-                                    [
-                                        eventBtn,
-                                        [CLOSE_REPORT],
-                                        {
-                                            tabindex: "-1",
-                                            class:
-                                                "fixed inset-0 h-full w-full cursor-default bg-black bg-opacity-50",
-                                        },
-                                    ],
-                                ]
-                              : [],
+                          reportOpen ? [reportModal] : [],
                       ],
             ],
             [
